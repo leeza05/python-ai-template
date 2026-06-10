5721bfcffd57fb5dca376d3e8cd4ee8b51a43953
Leeza Batra <leezabatra123@gmail.com>
2026-06-10 16:32:33 +0530

    first llm api call with groq

---
+++ python-ai-template/llm-chatbot/src/main.py
@@ -0,0 +1,16 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "user", "content": "What is a Large Language Model? Explain in 3 lines."}
+    ]
+)
+
+print(response.choices[0].message.content)
